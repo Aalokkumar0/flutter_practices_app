c581c58542ef6473cb06252c3a6546c12bc365d3
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-03-25 12:39:41 +0530

    adding a container and Text with Textstyle

--- lib/screen/precticeScreen.dart
+++ lib/screen/precticeScreen.dart
@@ -1,17 +1,41 @@
-// ignore: file_names
 import 'package:flutter/material.dart';
 
-class Precticescreen extends StatelessWidget{
+class Precticescreen extends StatelessWidget {
   const Precticescreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-    appBar: AppBar(
-      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-      title:Center(child: Text("Practicesapp",style: TextStyle(color: Colors.amber,fontSize: 30,fontWeight: FontWeight.w900),)),
-    ),
-   );
-
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        title: Center(
+          child: Text(
+            "Practicesapp",
+            style: TextStyle(
+              color: Colors.amber,
+              fontSize: 30,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          color: Colors.black,
+          child: Center(
+            child: Text(
+              "Hello' this is Container",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w900,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
